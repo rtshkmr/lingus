0239fce597a4457ae7c4a9ab1b47a0980a863139
Ritesh Kumar <ritesh@emerald.pink>
2021-01-03 20:42:54 +0800

    Minor Changes

--- hello.py
+++ hello.py
@@ -25,7 +25,6 @@ import os
 # creates a copy of the input .docx file into a .txt file and returns contents as an opened FileObject
 def initMe(initialFileName, newFileName):
   # create a new file to work on:
-  
   originalContent = docx2txt.process(initialFileName)
   newFileObject = open(newFileName, "w")
   # copy over to the new file name:
@@ -43,16 +42,39 @@ def outputFile(words, tags):
      string += str(word) + "---" + str(tag) + " \n"
   newFileObject.write(string)
 
+
+# 4 --> CC
+
 # tag validator:
-def isValidTag(tag): 
+def isTag(tag): 
+  # TODO: add in a dict of all the possible POS. if the tag is not in this dict, then return false
+  POSArray=["CC"]
   return tag != "." 
 
-# Gather our code in a main() function
-def main():
-  # note that the input file has to be in the same project directory
-  initialFileName = sys.argv[1]
-  newFileName = "workspace_" + initialFileName[:-5] + ".txt"
-  contents = initMe(initialFileName, newFileName)
+
+
+# each word will prompt an input to ask us if its in the correct category. if wrong, then asks helper to reassign the tag to a valid tag
+# returns a list: [correctWords, correctTags]
+def promptHuman(initialWords, initialTags):
+  correctWords, correctTags = [], []
+  size = len(initialWords)
+
+  # for i in range(size):
+  #   # check if it's a valid word -- tag assignment [goes through every word]
+  #   isValid = "Y" == input("Is this valid? \[ %s + %s \] \n \t type Y or N ", initialWords[i], initialTags[i]).capitalize
+  #   # if not, then
+  #   if not isValid:
+  #     # ask the human what is valid out of the
+  #     # they enter valid tag 
+  #     # we use isTag to check if its valid
+  #   else: 
+  #     correctWords.append(initialWords[i])
+  #     correctTags.append(initialTags[i])
+
+  return [correctWords, correctTags]
+
+# correctly assigning the POS to each word, returns a valid list of lists: [words, tags] in a list
+def checkPOS(contents):
   splitContents = contents.split(" ")
   size = len(splitContents)
   words = [size]
@@ -64,15 +86,28 @@ def main():
       splitTerm = term.split("_")
       word, tag  = splitTerm[0], splitTerm[1]
       # add in a check the tag should be valid
-      if(isValidTag(tag)):
-        print("iteration # %s with the word %s gives the word %s and has the tag %s" %(i, term, word, tag))
+      if(isTag(tag)):
+        print("iteration #%s with the term %s gives the word %s and has the tag %s" %(i, term, word, tag))
         words.append(word)
         tags.append(tag)
+  return promptHuman(words, tags)
+  # TODO: call a helper that shall make the human check if the current tag is logically correct or not, 
+  # each word will prompt an input to ask us if its in the correct category. if wrong, then asks helper to reassign the tag to a valid tag
+
+
+# Gather our code in a main() function
+def main():
+  # note that the input file has to be in the same project directory
+  initialFileName = sys.argv[1]
+  newFileName = "workspace_" + initialFileName[:-5] + ".txt"
+  contents = initMe(initialFileName, newFileName)
+  arr = checkPOS(contents)
+  words, tags = arr[0], arr[1]
   outputFile(words, tags)
-  print(words)
-  print(tags)
-  # print(contents)
-  # print(workingFileObject.read())
+
+
+
+
 
 # Standard boilerplate to call the main() function to begin
 # the program.
